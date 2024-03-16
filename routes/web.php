94dfc7cef4b6f6d0bf79da8b1ba9a39bef23227a
VivekSharma-32 <50068155+VivekSharma-32@users.noreply.github.com>
2024-03-16 22:28:03 +0530

    changes

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,3 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/', [CompanyController::class, 'index'])->name('companies.index');
-Route::get('/create', [CompanyController::class, 'create'])->name('companies.create');
-Route::post('/companies', [CompanyController::class, 'store'])->name('companies.store');
-Route::get('/edit/{id}', [CompanyController::class, 'edit'])->name('companies.edit');
-Route::put('/update', [CompanyController::class, 'update'])->name('companies.update');
-Route::post('/', [CompanyController::class, 'destroy'])->name('companies.destroy');
